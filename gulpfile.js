6068c7e156615eb977df4b6b6f15679f3351fcde
Yevgeny Skuridin <redfield.ru@gmail.com>
2015-03-01 19:24:30 +0500

    New gulpfile and plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,64 @@
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    changed     = require('gulp-changed'),
-    htmlmin     = require('gulp-htmlmin'),
-    cssshrink   = require('gulp-cssshrink'),
-    connect     = require('gulp-connect');
+var gulp = require('gulp'),
+    _p   = require('gulp-load-plugins')();
+
+_p.del = require('del');
+_p.fs  = require('fs');
+
+var SRC  = './src',
+    DIST = '.';
 
 gulp.task('html', function() {
-    gulp.src('src/*.html')
-        .pipe(changed('.'))
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('.'))
-        .pipe(connect.reload());
+  gulp.src(SRC + '/*.jade')
+      .pipe(_p.plumber())
+      .pipe(_p.jade({ data: JSON.parse(_p.fs.readFileSync('./data.json')) }))
+      .pipe(gulp.dest(DIST))
+      .pipe(_p.connect.reload());
 });
 
-gulp.task('images', function() {
-    gulp.src(['src/img/*.png', 'src/img/*.jpg', 'src/img/*.svg'])
-        .pipe(changed('.'))
-        .pipe(gulp.dest('./img'))
-        .pipe(connect.reload());
-});
+gulp.task('css', function() {
+  gulp.src(SRC + '/css/main.styl')
+      .pipe(_p.plumber())
+      .pipe(_p.stylus())
+      .pipe(_p.autoprefixer())
+      .pipe(_p.minifyCss())
+      .pipe(gulp.dest(DIST + '/css'))
+      .pipe(_p.connect.reload());
+})
 
-gulp.task('styles', function() {
-    gulp.src('./src/css/*.scss')
-        .pipe(changed('./css'))
-        .pipe(sass())
-        .pipe(cssshrink())
-        .pipe(gulp.dest('./css'))
-        .pipe(connect.reload());
-});
+gulp.task('js', function() {
+  gulp.src(SRC + '/js/*.js')
+      .pipe(_p.plumber())
+      .pipe(_p.uglify())
+      .pipe(gulp.dest(DIST + '/js'))
+      .pipe(_p.connect.reload());
+})
 
-gulp.task('connect', function() {
-    connect.server({
-        livereload: true,
-        port: 8000,
-        host: "0.0.0.0"
-    });
+gulp.task('img', function() {
+  gulp.src(SRC + '/img/**/*')
+      .pipe(_p.plumber())
+      .pipe(_p.imagemin({ progressive: true }))
+      .pipe(gulp.dest(DIST + '/img'))
+      .pipe(_p.connect.reload());
+})
+
+gulp.task('server', function() {
+  _p.connect.server({ root: './.', livereload: true });
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/css/*.scss', ['styles']);
-    gulp.watch('src/*.html', ['html']);
+  _p.watch(SRC + '/**/*.jade', function() { gulp.start('html'); });
+  _p.watch(SRC + '/css/*.styl', function() { gulp.start('css'); });
+  _p.watch(SRC + '/js/*.js', function() { gulp.start('js'); });
+  _p.watch(SRC + '/img/**/*', function() { gulp.start('img'); });
+  _p.watch('./data.json', function() { gulp.start('html') })
 });
 
-gulp.task('default', ['connect', 'watch', 'html', 'styles', 'images']);
-gulp.task('build', ['html', 'styles', 'images']);
+gulp.task('clean', function(cb) {
+  _p.del([DIST + '/css', DIST + '/img'], cb)
+})
+
+gulp.task('build', ['clean'], function() {
+  gulp.start(['html', 'css', 'js', 'img']);
+});
+
+gulp.task('default', ['build', 'server', 'watch']);
