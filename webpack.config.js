09b7e05ebc1613d08a3b6fe4f631f2edd0efca13
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-07-18 13:43:36 +0500

    Install babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
   ],
   module: {
     loaders: [
+      { test: /\.jsx$/, loader: 'babel' },
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract("css!autoprefixer")
