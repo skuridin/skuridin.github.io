6bce361f964d420f9d88bf420c529606dfd450f1
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-07-19 19:37:57 +0500

    Return function for renderToString

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,18 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes.jsx');
 
-Router.run(routes, Router.HashLocation, function(Handler) {
-  React.render(<Handler />, document.getElementById('app'));
-});
+if(typeof(window) !== 'undefined') {
+  Router.run(routes, Router.HashLocation, function(Handler) {
+    React.render(<Handler />, document.getElementById('app'));
+  });
+}
+
+var renderToString = function() {
+  var result;
+  Router.run(routes, '/', function(Handler) {
+    result = React.renderToString(<Handler />);
+  })
+  return result;
+};
+
+module.exports = renderToString;
