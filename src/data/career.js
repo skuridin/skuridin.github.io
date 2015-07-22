4457aa87075455dccf0507081dbf1201d25cc169
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-07-22 22:51:34 +0500

    Career data file

---
+++ src/data/career.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'Comnet ISP',
+    position: 'Full Stack Developer',
+    period: 'Jan. 2009 — Jun. 2010',
+    text: ''
+  },
+  {
+    title: 'Brandbox Branding',
+    position: 'Full Stack Developer',
+    period: 'Jun. 2010 — Jun. 2011',
+    text: ''
+  }
+];
