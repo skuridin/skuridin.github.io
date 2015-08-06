f4dcce65664fb94a246f82cdfb21218abde173f3
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-08-06 17:13:32 +0500

    Delete all react

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,20 @@
-var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var Clean = require('clean-webpack-plugin');
-var htmlFileName = 'index.html';
-
-if(process.env.NODE_ENV === 'production') htmlFileName = 'template.html';
 
 module.exports = {
   context: __dirname + '/src',
-  entry: './main.jsx',
+  entry: './app.js',
   output: {
     path: __dirname,
     filename: 'assets/bundle.js',
-    libraryTarget: 'umd',
     publicPath: '/'
-  },
-  resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
   },
   plugins: [
     new Clean(['assets', './*.html']),
     new ExtractTextPlugin('assets/style.css'),
-    new webpack.DefinePlugin({
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-    }),
     new HtmlWebpackPlugin({
       template: './src/template.html',
-      filename: htmlFileName,
       title: 'Evgeniy Skuridin',
       favicon: 'favicon.ico',
       hash: true,
@@ -37,26 +25,18 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.jsx$/, loader: 'babel' },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('css!autoprefixer')
-      },
       {
         test: /\.styl$/,
         loader: ExtractTextPlugin.extract('css!autoprefixer!stylus')
       },
+      { test: /\.hbs$/, loader: 'handlebars' },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
             'file?name=assets/[name].[ext]',
             'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
         ]
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/i,
-        loader: 'file?name=assets/[name].[ext]'
-      },
+      }
     ]
   }
 };
