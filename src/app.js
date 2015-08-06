da1263a7466494e97108f4659a9b5181cf8531d7
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-08-06 18:31:38 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 require('./style.styl');
+var homeTemplate = require('./home.hbs');
+
+var page = {
+  title: 'Evgeniy Skuridin — Front End Developer',
+  image: require('./images/logo.png'),
+  info: 'Hi! My name is Evgeniy. Frontend developer. Tashkent, Uzbekistan. ' +
+    'Ready to relocate.',
+  links: [
+    { title: 'CV', url: 'Evgeniy.Skuridin.Frontend.pdf' },
+    { title: 'Github', url: 'https://github.com/skuridin' },
+    { title: 'Email', url: 'mailto:i@skurid.in' },
+    { title: 'Skype', url: 'skype:redfield1990?chat' },
+  ]
+};
+
+document.getElementById('app').innerHTML = homeTemplate(page);
