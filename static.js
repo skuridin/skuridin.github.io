ce12f371f32867d5b680859a7b2f685832214ae7
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-08-10 18:27:54 +0500

    Clean

--- static.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var minify = require('html-minifier').minify;
-var Handlebars = require('handlebars');
-var content = require('./assets/bundle');
-var template = __dirname + '/index.html';
-var markup = fs.readFileSync(template, 'utf8');
-markup = Handlebars.compile(markup);
-markup = markup({ content: minify(content, { collapseWhitespace: true }) });
-fs.writeFileSync(template, markup);
