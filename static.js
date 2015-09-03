624e65b20d170a878f14ea7b44e8c0faebb35fc2
Evgeniy Skuridin <redfield.ru@gmail.com>
2015-09-04 00:11:17 +0500

    Simple html

--- static.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var template = __dirname + '/template.html';
-var render = require('./dist/bundle');
-var markup = fs.readFileSync(template, 'utf-8');
-render('/', function(result, pageTitle) {
-  markup = markup.replace('{react}', result);
-  markup = markup.replace('{pageTitle}', pageTitle);
-  fs.writeFileSync(__dirname + '/index.html', markup);
-});
