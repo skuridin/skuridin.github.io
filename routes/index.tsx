bc35f8677f8a7ecf359474c3df1d3f73772456cb
Evgeniy Skuridin <redfield.ru@gmail.com>
2022-07-19 17:45:35 +0200

    feat: more accent on linkedin

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,8 +4,6 @@ import { apply, tw } from "@twind";
 import { Head } from "$fresh/runtime.ts";
 
 const SKILLS = ["GraphQL", "React", "TypeScript"];
-const skillBadgeStyles = apply
-	`inline-flex bg-black text-white rounded px-2 py-1`;
 
 export default function Home() {
 	return (
@@ -26,24 +24,30 @@ export default function Home() {
 						Hi, <br />
 						I'm Evgeniy, <br />
 						Frontend Developer <br />
-						located in the Netherlands.
+						based in the Netherlands.
 						<span
 							class={tw
-								`inline-flex absolute inset-0 bg-gradient-to-r from-red-600 to-blue-600 pointer-events-none mix-blend-lighten`}
+								`inline-flex absolute inset-0 bg-gradient-to-b from-red-600 to-blue-600 pointer-events-none mix-blend-lighten`}
 						/>
 					</h1>
 					<ul class={tw`flex items-center gap-x-4 mt-4 pt-4 border-t-1`}>
 						{SKILLS.map((skill) => (
-							<li key={skill} class={tw`${skillBadgeStyles}`}>{skill}</li>
+							<li
+								key={skill}
+								class={tw
+									`inline-flex rounded px-2 h-6 bg-gray-100 items-center text-xs`}
+							>
+								{skill}
+							</li>
 						))}
 					</ul>
 					<div class={tw`mt-4 pt-4 border-t-1`}>
-						You can find me on{" "}
 						<a
 							href="https://www.linkedin.com/in/redfield1990/"
-							class={tw`underline text-blue-400 hover:text-blue-800`}
+							class={tw
+								`inline-flex text-white rounded bg-blue-700 hover:bg-blue-800 h-10 px-4 items-center`}
 						>
-							LinkedIn
+							Contact me on LinkedIn
 						</a>
 					</div>
 				</div>
