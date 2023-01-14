a3dd9d9397a30cfff0a5b7c08c3d3653e11a4e6b
Eugene Skuridin <i@skurid.in>
2023-01-14 10:53:52 +0100

    use clsx to manage twind classes

--- features/intro/About/About.tsx
+++ features/intro/About/About.tsx
@@ -1,10 +1,22 @@
+import { clsx } from "clsx";
+
 export default function About() {
 	return (
-		<h1 class="text-3xl bg-gradient-to-b from-red-600 to-blue-600 bg-clip-text text-transparent">
+		<h1
+			class={clsx([
+				"text-3xl bg-clip-text text-transparent",
+				"bg-gradient-to-br from-red-600 to-blue-600 bg-right",
+				"transition-all duration-500",
+				"hover:bg-left",
+			])}
+			style={{
+				backgroundSize: "400%",
+			}}
+		>
 			Hi, <br />
 			I'm Eugene, <br />
 			Frontend Developer <br />
-			based in the Netherlands
+			Based in the Netherlands
 		</h1>
 	);
 }
--- features/intro/LinkedInLink/LinkedInLink.tsx
+++ features/intro/LinkedInLink/LinkedInLink.tsx
@@ -1,10 +1,18 @@
+import { clsx } from "clsx";
+
 export default function LinkedInLink() {
 	return (
 		<a
 			href="https://www.linkedin.com/in/redfield1990/"
-			class="inline-flex text-white rounded bg-blue-700 hover:bg-blue-800 h-12 px-4 items-center text-xl active:translate-y-1 transition-all"
+			class={clsx([
+				"inline-flex h-12 px-4 items-center",
+				"rounded",
+				"bg-blue-700 hover:bg-blue-800",
+				"text-white text-xl",
+				"active:translate-y-1 transition-all",
+			])}
 		>
-			Contact me on LinkedIn
+			My LinkedIn profile
 		</a>
 	);
 }
